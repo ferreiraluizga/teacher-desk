9d46111fe02b72093a1cd6cf40e390f0deafdd17
Luiz Gabriel Ferreira <luiz.gabriel.lcf@gmail.com>
2025-05-10 10:12:16 -0300

    feat: modeling routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\AverageController;
+use App\Http\Controllers\ClassModelController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('layouts.app');
 });
+
+Route::resource('/students', StudentController::class);
+
+Route::resource('/classes', ClassModelController::class);
+
+Route::resource('/averages', AverageController::class);
