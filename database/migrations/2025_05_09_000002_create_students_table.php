bdaf6277a5003e011914d8bbc717dfb8391b1765
Luiz Gabriel Ferreira <luiz.gabriel.lcf@gmail.com>
2025-05-11 23:09:02 -0300

    feat: controller for Courses created

--- database/migrations/2025_05_09_000002_create_students_table.php
+++ database/migrations/2025_05_09_000002_create_students_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('description');
             $table->date('birth');
-            $table->foreignId('class_id')->constrained('classes')->onDelete('cascade');
+            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
             $table->timestamps();
         });
     }
