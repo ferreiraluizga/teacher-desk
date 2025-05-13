aa84d261503bb6474ec8b4325e5c901988061977
Luiz Gabriel Ferreira <luiz.gabriel.lcf@gmail.com>
2025-05-13 20:31:31 -0300

    fix: adding "use Has Factory" in models

--- app/Models/Average.php
+++ app/Models/Average.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Average extends Model
 {
+
+    use HasFactory;
+
     protected $table = 'averages';
 
     protected $fillable = [
--- database/migrations/2025_05_09_000002_create_students_table.php
+++ database/migrations/2025_05_09_000002_create_students_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->date('birth');
             $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
             $table->timestamps();
