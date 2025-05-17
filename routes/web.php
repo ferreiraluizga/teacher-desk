c8e8559f9b462c5d330c7f7a55e2aa2096411969
Luiz Gabriel Ferreira <luiz.gabriel.lcf@gmail.com>
2025-05-17 17:54:46 -0300

    build: installing laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,20 @@
 <?php
 
-use App\Http\Controllers\AverageController;
-use App\Http\Controllers\CourseController;
-use App\Http\Controllers\StudentController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
-})->name('home');
+    return view('welcome');
+});
 
-Route::resource('/courses', CourseController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('/students', StudentController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::resource('/averages', AverageController::class);
+require __DIR__.'/auth.php';
